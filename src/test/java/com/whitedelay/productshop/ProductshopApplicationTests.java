21d9fa5cd288cf9e6b72174b407e04c5b5ed9188
delay-100 <gitignore@kakao.com>
2024-06-28 23:45:49 +0900

    FEAT : test코드 삭제

--- src/test/java/com/whitedelay/productshop/ProductshopApplicationTests.java
+++ src/test/java/com/whitedelay/productshop/ProductshopApplicationTests.java
@@ -3,7 +3,7 @@ package com.whitedelay.productshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProductshopApplicationTests {
 
 	@Test
