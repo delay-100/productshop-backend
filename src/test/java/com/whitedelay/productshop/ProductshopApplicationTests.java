57b43428e0e8c50b7ef5e2655022166c4cae6d1e
delay-100 <gitignore@kakao.com>
2024-06-29 09:30:52 +0900

    FEAT : @SpringBootTest class명 추가

--- src/test/java/com/whitedelay/productshop/ProductshopApplicationTests.java
+++ src/test/java/com/whitedelay/productshop/ProductshopApplicationTests.java
@@ -3,11 +3,15 @@ package com.whitedelay.productshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = ProductshopApplicationTests.class)
 class ProductshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
+
 	}
 
 }
