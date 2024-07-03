24771ddcd7f6a216fa16c67c90ecc8cd41972027
delay-100 <gitignore@kakao.com>
2024-07-04 08:36:13 +0900

    FEAT: 회원가입 dto내 Builder추가

--- src/main/java/com/whitedelay/productshop/mail/dto/SignupVerificationEmailDto.java
+++ src/main/java/com/whitedelay/productshop/mail/dto/SignupVerificationEmailDto.java
@@ -1,8 +1,14 @@
 package com.whitedelay.productshop.mail.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignupVerificationEmailDto {
     private String email;
 }
--- src/main/java/com/whitedelay/productshop/mail/dto/SignupVerifyCodeDto.java
+++ src/main/java/com/whitedelay/productshop/mail/dto/SignupVerifyCodeDto.java
@@ -1,8 +1,14 @@
 package com.whitedelay.productshop.mail.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignupVerifyCodeDto {
     private String emailCode;
     private String email;
