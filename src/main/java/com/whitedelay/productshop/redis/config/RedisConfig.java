2b0e3df17dbd3e90a2655cd63b579de064f9991f
delay-100 <gitignore@kakao.com>
2024-07-04 07:25:25 +0900

    FIX : RedisTemplate를 primary로 설정(stringRedisTemplate과 bean 형식이 같음)

--- src/main/java/com/whitedelay/productshop/redis/config/RedisConfig.java
+++ src/main/java/com/whitedelay/productshop/redis/config/RedisConfig.java
@@ -2,7 +2,9 @@ package com.whitedelay.productshop.redis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -10,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
